09c10f0a7e03c52ae20f05f887eed279a5895602
Wiz-TechSkillsIT <wiztechskillsit@gmail.com>
2025-02-18 15:49:55 +0530

    case study -- gadgets

---
+++ cs_api.js
@@ -0,0 +1,27 @@
+/**
+ * Call the API
+ * https://api.restful-api.dev/objects
+ * that will give you list of all the gadgets that we sell on our 
+ * platform
+ * [
+  {
+    "id": "1",
+    "name": "Google Pixel 6 Pro",
+    "data": {
+      "color": "Cloudy White",
+      "capacity": "128 GB"
+    }
+  },
+  {
+    "id": "2",
+    "name": "Apple iPhone 12 Mini, 256GB, Blue",
+    "data": null
+  }
+]
+
+-- create a function to display all record in following structure 
+{id,name,color,price} <-- display these 3 properties from objects
+-- create a function to display records based on particular id
+getRecordById(4/5/6) 
+if you do not have any matching record then display "Invalid ID " in the terminal 
+*/
