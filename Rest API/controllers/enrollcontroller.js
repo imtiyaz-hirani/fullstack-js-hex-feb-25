921cb8b841526885ca225bf5a864dc1ab4b72a6c
Wiz-TechSkillsIT <wiztechskillsit@gmail.com>
2025-02-21 12:27:26 +0530

    get all enrollments api

--- Rest API/controllers/enrollcontroller.js
+++ Rest API/controllers/enrollcontroller.js
@@ -3,30 +3,46 @@ const Enrollment = require("../models/enrollment");
 const Student = require("../models/student");
 
 exports.enrollStudentInCourse = async (req,res)=>{
-    //console.log('enroll api called....');
-    const {sid,cid} = req.body;
-    //console.log(`student id: ${sid} and course id: ${cid}`)
+    try {
+        //console.log('enroll api called....');
+        const { sid, cid } = req.body;
+        //console.log(`student id: ${sid} and course id: ${cid}`)
 
-    //step 1: validate sid and cid 
-    let student = await Student.findById(sid); 
+        //step 1: validate sid and cid 
+        let student = await Student.findById(sid);
 
-    if(!student){ //if student is not found/defined 
-       return res.status(400).json({'msg': 'student id invalid!!'});
+        if (!student) { //if student is not found/defined 
+            return res.status(400).json({ 'msg': 'student id invalid!!' });
+        }
+
+        let course = await Course.findById(cid);
+
+        if (!course) {
+            return res.status(400).json({ 'msg': 'course id invalid!!' });
+        }
+
+        //step 2 : create enroll obj and save it in db
+        let enrollObj = {
+            student: sid,
+            course: cid
+        }
+        let enroll = new Enrollment(enrollObj);
+        enroll = await enroll.save();
+
+        res.status(200).json(enroll);
     }
-
-    let course = await Course.findById(cid);
-
-    if(!course){
-        return res.status(400).json({'msg': 'course id invalid!!'});
+    catch (err) {
+        res.status(400).json({ 'msg': `ERROR in api: ${err}` })
     }
+    
+}
 
-    //step 2 : create enroll obj and save it in db
-    let enrollObj = {
-        student: sid,
-        course: cid
+exports.fetchAllEnrollments= async (req,res)=>{
+    try{
+        const enrollments = await Enrollment.find().populate("student").populate("course");
+        res.status(200).json(enrollments);
     }
-    let enroll = new Enrollment(enrollObj); 
-    enroll = await enroll.save();
-
-    res.status(200).json(enroll); 
+    catch(err){
+        res.status(400).json({ 'msg': `ERROR in api: ${err}` })
+    }
 }
--- Rest API/routes/enrollRoute.js
+++ Rest API/routes/enrollRoute.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const { enrollStudentInCourse } = require('../controllers/enrollcontroller');
+const { enrollStudentInCourse, fetchAllEnrollments } = require('../controllers/enrollcontroller');
 
 const router = express.Router();
 
 router.post('/add',enrollStudentInCourse)
+router.get('/all',fetchAllEnrollments)
+
+
 
 module.exports=router; 
 
